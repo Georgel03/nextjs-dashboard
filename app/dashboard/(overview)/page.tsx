259634163ad9617abed7e52b9d5b58c19c8ab7ba
Georgel03 <gstance@ymail.com>
2025-12-02 13:47:45 +0200

    Implemented CRUD operations also with validation and error handling

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,8 +8,6 @@ import CardWrapper from '@/app/ui/dashboard/cards';
  
 export default async function Page() {
  
-  //const latestInvoices = await fetchLatestInvoices();
-  // const {numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices} = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
